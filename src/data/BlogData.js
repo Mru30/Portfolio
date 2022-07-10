6fc976e9add3c00c6cdae7dc1ec50626fba43398
Mru30 <108016061+Mru30@users.noreply.github.com>
2022-07-10 15:50:45 +0530

    Add files via upload

---
+++ src/data/BlogData.js
@@ -0,0 +1,43 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Anantya Banner Design",
+        tags:["Canva","OpenSea","NFT","Metaverse"],
+        date:"22nd and 23rd March 2022",
+        imgSrc:"https://i.im.ge/2022/07/10/upEl1J.png",
+        link:"https://www.canva.com/design/DAE6k_VFDUc/_GoaEk015phdFIQ_jNsOZQ/view"
+    },
+    {
+        id:2,
+        name:"NFT Buggu Shark",
+        tags:["Canva","OpenSea","NFT","Metaverse"],
+        
+        imgSrc:"https://i.im.ge/2022/07/10/upAZqa.png",
+        link:"https://www.canva.com/design/DAFF9ReqdAk/CZY9S3Ei57EqlxRJv-assw/view"
+    },
+    {
+        id:3,
+        name:"NFT Nariyal Anna",
+        tags:["Canva","OpenSea","NFT","Metaverse"],
+
+        imgSrc:"https://i.im.ge/2022/07/10/upCTFY.png",
+        link:"https://www.canva.com/design/DAFF9WYM1BY/tSdTSBdESMCyBiMtNP3r3w/view#1"
+    },
+    {
+        id:4,
+        name:"Sketches",
+        tags:["Pencil Sketch","Artwork"],
+        imgSrc:"https://i.im.ge/2022/07/10/upCdzm.png",
+        link:"https://www.canva.com/design/DAFF9b8B0s8/SMGOGpPkfeQ_4NlhBXC70g/view"
+    },
+    
+    {
+        id:5,
+        name:"Paintings",
+        tags:["Water Colors","Artwork"],
+        imgSrc:"https://i.im.ge/2022/07/10/upCO4P.png",
+        link:"https://www.canva.com/design/DAFF9TM63eo/8BPvPRLt5ovIT_HDMnRb-g/view"
+    },
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,55 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Karyam",
+        description:"It is Internship and Job Portal build on top of the React JS, with styledComponents.",
+        tags:["react","styledComponents"],
+        
+        demo:"http://www.maps2.in",
+        github:"https://github.com/Mru30/Karyam_Portal"
+    },
+    {
+        id:2,
+        name:"Hogwarts University Website",
+        description:"It is a Hogwarts University Website listing various information.",
+        tags:["HTML5","CSS3","Javascript","Bootstrap","JQuery"],
+        
+        demo:"https://mru2001.github.io/Hogwarts-complete-website/",
+        github:"https://github.com/Mru2001/Hogwarts-complete-website"
+        
+    },
+    {
+        id:3,
+        name:"Covid-19 Live Tracker",
+        description:"Created a Covid-19 Live Tracker using ReactJs and Rest API.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://mru2001.github.io/Covid-19-Live-Tracker/",
+        github:"https://github.com/Mru2001/Covid-19-Live-Tracker"
+    },
+    {
+        id:4,
+        name:"DrumStick",
+        description:"Created a Drumstick game.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://mru2001.github.io/Drumkit/",
+        github:"https://github.com/Mru2001/Drumkit"
+       
+    },
+    {
+        id:5,
+        name:"Roll-The-Dice",
+        description:"Created two Dice Roller.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://mru2001.github.io/Roll-the-Dice/",
+        github:"https://github.com/Mru2001/Roll-the-Dice"
+       
+    }
+    
+    
+  
+] 
